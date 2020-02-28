f0a8907d02a30d62bee88dbfe18cf32eb2505ef5
foldu <foldu@protonmail.com>
2020-02-28 17:04:17 +0100

    Put completions in out_dir

--- build.rs
+++ build.rs
@@ -1,14 +1,16 @@
 include!("src/opt.rs");
 
 const BIN: &str = "tgc";
-const TARGET: &str = "target/completions";
 
 fn main() {
     let mut opt = Opt::clap();
 
-    std::fs::create_dir_all(TARGET).expect("Can't create target dir");
+    let outdir = match std::env::var_os("OUT_DIR") {
+        None => return,
+        Some(outdir) => outdir,
+    };
 
-    opt.gen_completions(BIN, structopt::clap::Shell::Zsh, TARGET);
-    opt.gen_completions(BIN, structopt::clap::Shell::Bash, TARGET);
-    opt.gen_completions(BIN, structopt::clap::Shell::Fish, TARGET);
+    opt.gen_completions(BIN, structopt::clap::Shell::Zsh, &outdir);
+    opt.gen_completions(BIN, structopt::clap::Shell::Bash, &outdir);
+    opt.gen_completions(BIN, structopt::clap::Shell::Fish, &outdir);
 }
